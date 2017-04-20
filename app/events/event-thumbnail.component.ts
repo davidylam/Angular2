706bbd376adf0964848c59aeccc71ae45e85d565
David Lam <DavidLam@DecisionResearch.com>
2017-04-19 21:42:04 -1000

    Half way thru Module 4. ngIf, ngFor, save-navigation

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,19 +3,21 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
 @Component({
     selector: 'event-thumbnail',
     template: `
-     <div class="well hoverwell thumbnails">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}} </span>
+     <div class="well hoverwell thumbnail">
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}} </span>
             <span class="pad-left">
-            {{event.location.city}}, {{event.location.country}}</span>
+            {{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
+        <div *ngIf="event?.onlineUrl">Online Url: {{event?.onlineUrl}}</div>
     </div>
     `,
     styles: [`
+        .thumbnail { min-height: 210px;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb";}
     `]
