a27af2a2d6a1dcb47c72b56fac8372011bb6aaac
David Lam <David.Lam@DecisionResearch.com>
2017-04-21 14:51:21 -1000

    move events folder

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,13 @@ import {EventsAppComponent} from './events-app.component'
 import {EventsListComponent} from './events/events-list.component'
 import {EventThumbnailComponent} from './events/event-thumbnail.component'
 import {NavBarComponent} from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
 
 
 @NgModule ({
     imports: [BrowserModule],
     declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent], 
+    providers: [ EventService],
     bootstrap: [EventsAppComponent],
 })
 
