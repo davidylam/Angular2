e99afd41a266dec7ec2f46ba6e6587ba506caad5
David Lam <David.Lam@DecisionResearch.com>
2017-05-01 09:47:56 -1000

    Completed Module 11. Dependency Injection on Toastr

--- app/common/toastr.service.ts
+++ app/common/toastr.service.ts
@@ -1,21 +1,10 @@
-import { Injectable } from '@angular/core'
-declare let toastr: any
+import { OpaqueToken } from '@angular/core'
 
-@Injectable()
-export class ToastrService {
-    success(message: string, title?: string){
-        toastr.success(message, title)
-    }
+export let TOASTR_TOKEN = new OpaqueToken('toastr');
 
-    info(message: string, title?: string){
-        toastr.info(message, title)
-    }
-
-    warning(message: string, title?: string){
-        toastr.warning(message, title)
-    }
-
-    error(message: string, title?: string){
-        toastr.error(message, title)
-    }
+export interface IToastr{
+    success(msg: string, title?: string):void;
+    info(msg: string, title?: string):void;
+    warning(msg: string, title?: string):void;
+    error(msg: string, title?: string):void;
 }
